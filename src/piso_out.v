3323195bbe7859d074e3f5ce8c04074744316b45
Matheus <matheusmat88@gmail.com>
2025-08-31 13:01:59 -0300

    Add PISO_OUT module and corresponding testbench for functionality verification

---
+++ src/piso_out.v
@@ -0,0 +1,35 @@
+module PISO_OUT #(
+    parameter WIDTH = 8,
+    parameter NUM_TAPS = 4
+)(
+    input                             CLR_PISO_OUT,
+    input                             CLKEXT,
+    input                             SHIFT_OUT,
+    input                             EN_PISO_OUT,
+    input  [WIDTH*NUM_TAPS-1:0] DATA_IN,
+    output [WIDTH-1:0]                DATA_OUT
+);
+
+    reg [WIDTH-1:0] reg_data [0:NUM_TAPS-1];
+    
+
+    assign DATA_OUT = reg_data[NUM_TAPS-1];
+
+    always @(posedge CLKEXT or posedge CLR_PISO_OUT)begin
+        integer i;
+        if(CLR_PISO_OUT) begin
+            for(i=0; i<NUM_TAPS; i=i+1) begin
+                reg_data[i] <= 0;
+            end
+        end else if (!SHIFT_OUT) begin
+            for(i=0; i<NUM_TAPS; i=i+1) begin
+                reg_data[i] <= DATA_IN[WIDTH*i +: WIDTH];
+            end
+        end else if (EN_PISO_OUT) begin
+            //reg_data[NUM_TAPS-1] <= reg_data[NUM_TAPS-2];
+            for(i=NUM_TAPS-1; i>0; i=i-1) begin
+                reg_data[i] <= reg_data[i-1];
+            end
+        end
+    end
+endmodule
---
+++ tb/tb_piso_out.v
@@ -0,0 +1,64 @@
+`timescale 1ns/1ps
+
+module tb_PISO_OUT;
+
+    parameter WIDTH = 8;
+    parameter NUM_TAPS = 4;
+
+    reg CLKEXT, CLR_PISO_OUT, SHIFT_OUT, EN_PISO_OUT;
+    reg [WIDTH*NUM_TAPS-1:0] DATA_IN;
+    wire [WIDTH-1:0] DATA_OUT;
+
+    PISO_OUT #(
+        .WIDTH(WIDTH),
+        .NUM_TAPS(NUM_TAPS)
+    ) uut (
+        .CLR_PISO_OUT(CLR_PISO_OUT),
+        .CLKEXT(CLKEXT),
+        .SHIFT_OUT(SHIFT_OUT),
+        .EN_PISO_OUT(EN_PISO_OUT),
+        .DATA_IN(DATA_IN),
+        .DATA_OUT(DATA_OUT)
+    );
+
+    // Clock generation
+    initial CLKEXT = 0;
+    always #5 CLKEXT = ~CLKEXT;
+
+    initial begin
+        CLR_PISO_OUT = 1;
+        SHIFT_OUT = 0;
+        EN_PISO_OUT = 0;
+        DATA_IN = 0;
+        #12;
+        CLR_PISO_OUT = 0;
+
+        // Carrega dados paralelos
+        DATA_IN = {8'hAA, 8'hBB, 8'hCC, 8'hDD}; // reg_data[3]=AA, [2]=BB, [1]=CC, [0]=DD
+        SHIFT_OUT = 0;
+        EN_PISO_OUT = 0;
+        #10;
+
+        // Ativa shift
+        SHIFT_OUT = 1;
+        EN_PISO_OUT = 1;
+        #10;
+        #10;
+        #10;
+        #10;
+
+        // Reset durante operação
+        CLR_PISO_OUT = 1;
+        #10;
+        CLR_PISO_OUT = 0;
+        #10;
+
+        $finish;
+    end
+
+    initial begin
+        $monitor("T=%0t | CLR=%b SHIFT=%b EN=%b DATA_IN=%h | DATA_OUT=%h", 
+            $time, CLR_PISO_OUT, SHIFT_OUT, EN_PISO_OUT, DATA_IN, DATA_OUT);
+    end
+
+endmodule
